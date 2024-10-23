282c3ca45d91cdbacc47d22704533feaef3b7384
YashZ2002 <zaveri.y@northeastern.edu>
2024-10-23 18:16:53 -0400

    RDS initial commit.

--- variable.tf
+++ variable.tf
@@ -18,20 +18,62 @@ variable "availability_zones" {
   description = "List of availability zones"
 }
 
-# EC2 Instance
 variable "instance_type" {
   default     = "t2.micro"
   description = "Instance type for EC2"
 }
 
-
 variable "application_port" {
   type        = number
+  default     = 5000 # Change to the port your application uses
   description = "Port for application"
-  default     = 8080 # Change to the port your application uses
 }
 
 variable "custom_ami_id" {
-  description = "The ID of the custom AMI to be used for the EC2 instance"
   type        = string
+  description = "The ID of the custom AMI to be used for the EC2 instance"
+}
+
+variable "key_name" {
+  description = "Key pair to be used for SSH access to EC2 instances"
+  default     = null
+}
+
+
+variable "root_volume_size" {
+  type        = number
+  default     = 25
+  description = "The size of the root volume for the EC2 instance"
+}
+
+
+
+variable "db_user" {
+  type        = string
+  default     = ""
+  description = "Database username."
+}
+
+variable "db_password" {
+  type        = string
+  default     = ""
+  description = "Database password."
+}
+
+variable "db_name" {
+  type        = string
+  default     = ""
+  description = "Database name."
+}
+
+variable "db_port" {
+  type        = number
+  default     = 3306
+  description = "Database port number."
+}
+
+variable "environment_tag" {
+  type        = string
+  default     = ""
+  description = "Environment tag for resources."
 }
