2ff086b9d94456e24fb9530f3c44a81c91a9d545
YashZ2002 <zaveri.y@northeastern.edu>
2024-11-13 11:19:43 -0500

    Auto scalling A07. update the fmt

--- main.tf
+++ main.tf
@@ -515,6 +515,7 @@ resource "aws_security_group" "lb_security_group" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   ingress {
@@ -522,6 +523,7 @@ resource "aws_security_group" "lb_security_group" {
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
@@ -529,6 +531,7 @@ resource "aws_security_group" "lb_security_group" {
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
